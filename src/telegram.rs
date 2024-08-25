f72bc19d00af4546374f9c43f77d37e81c885d21
lilydjwg <lilydjwg@gmail.com>
2024-08-25 15:11:17 +0800

    add is_telegram_str

--- src/telegram.rs
+++ src/telegram.rs
@@ -1,16 +1,22 @@
-use std::sync::LazyLock;
-
 use matrix_sdk::ruma;
 
-static TELEGRAM_BRIDGES: LazyLock<Vec<&'static ruma::ServerName>> = LazyLock::new(|| vec![
-  ruma::server_name!("nichi.co"),
-  ruma::server_name!("t2bot.io"),
-  ruma::server_name!("elv.sh"),
-  ruma::server_name!("moe.cat"),
-  ruma::server_name!("neo.angry.im"),
-]);
+static TELEGRAM_BRIDGES: &[&str] = &[
+  "nichi.co",
+  "t2bot.io",
+  "elv.sh",
+  "moe.cat",
+  "neo.angry.im",
+];
 
 pub fn is_telegram(uid: &ruma::UserId) -> bool {
-  TELEGRAM_BRIDGES.contains(&uid.server_name())
+  TELEGRAM_BRIDGES.contains(&uid.server_name().as_str())
     && uid.localpart().starts_with("telegram_")
+}
+
+pub fn is_telegram_str(uid: &str) -> bool {
+  if let Some((user, server)) = uid.rsplit_once(':') {
+    TELEGRAM_BRIDGES.contains(&server) && user.starts_with("@telegram_")
+  } else {
+    false
+  }
 }
