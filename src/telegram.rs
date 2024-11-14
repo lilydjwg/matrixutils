7b4be50473d4ce3ccef12fcd3c7515cf29b74bcf
lilydjwg <lilydjwg@gmail.com>
2024-11-14 14:31:51 +0800

    update telegram bridges

--- src/telegram.rs
+++ src/telegram.rs
@@ -8,14 +8,21 @@ static TELEGRAM_BRIDGES: &[&str] = &[
   "neo.angry.im",
 ];
 
+static TELEGRAM_BRIDGES_2: &[&str] = &[
+  "tether.kimiblock.top",
+];
+
 pub fn is_telegram(uid: &ruma::UserId) -> bool {
-  TELEGRAM_BRIDGES.contains(&uid.server_name().as_str())
-    && uid.localpart().starts_with("telegram_")
+  (TELEGRAM_BRIDGES.contains(&uid.server_name().as_str())
+   && uid.localpart().starts_with("telegram_"))
+  || (TELEGRAM_BRIDGES_2.contains(&uid.server_name().as_str())
+    && uid.localpart().starts_with("tg_"))
 }
 
 pub fn is_telegram_str(uid: &str) -> bool {
   if let Some((user, server)) = uid.rsplit_once(':') {
-    TELEGRAM_BRIDGES.contains(&server) && user.starts_with("@telegram_")
+    (TELEGRAM_BRIDGES.contains(&server) && user.starts_with("@telegram_"))
+      || (TELEGRAM_BRIDGES_2.contains(&server) && user.starts_with("@tg_"))
   } else {
     false
   }
