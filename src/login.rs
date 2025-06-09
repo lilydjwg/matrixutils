7aaaad3590164baf0c1434985ceee427abfd5f39
lilydjwg <lilydjwg@gmail.com>
2025-06-09 21:08:05 +0800

    support for password login

--- src/login.rs
+++ src/login.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 use std::io::Write;
+use std::sync::LazyLock;
 
 use matrix_sdk::ruma;
 use matrix_sdk::{
@@ -8,6 +9,7 @@ use matrix_sdk::{
   authentication::SessionTokens,
   SessionMeta,
 };
+use secrecy::ExposeSecret;
 use tracing::info;
 use eyre::Result;
 
@@ -51,6 +53,47 @@ pub async fn sso_login<P: AsRef<Path>>(
   Ok(client)
 }
 
+fn ask_password(account: &ruma::UserId) -> Result<Option<secrecy::SecretString>> {
+  if let Some(mut input) = pinentry::PassphraseInput::with_default_binary() {
+    Ok(Some(input
+      .with_title("Input Password")
+      .with_description(&format!("Enter password for account: {account}"))
+      .required("password can't be empty")
+      .interact().map_err(|e| eyre::eyre!("pinentry error: {e}"))?
+    ))
+  } else {
+    Ok(None)
+  }
+}
+
+pub async fn interactive_login<P: AsRef<Path>>(
+  loginfile: P,
+  uid: &ruma::UserId,
+) -> Result<Client> {
+  info!("interactive login.");
+
+  let client = build_client(Some(uid.server_name()), None).await?;
+  let password = if let Some(p) = ask_password(uid)? {
+    p
+  } else {
+    let mut rl = rustyline::DefaultEditor::new()?;
+    secrecy::SecretString::new(rl.readline("Password: ")?.into())
+  };
+  client.matrix_auth().login_username(uid, password.expose_secret()).send().await?;
+
+  let login_info = LoginInfo {
+    homeserver: client.homeserver(),
+    user_id: String::from(client.user_id().unwrap().as_str()),
+    device_id: String::from(client.device_id().unwrap().as_str()),
+    access_token: client.access_token().unwrap(),
+  };
+  let data = serde_json::to_string_pretty(&login_info)?;
+  let mut f = std::fs::File::create(loginfile)?;
+  f.write_all(data.as_bytes())?;
+  f.write_all(b"\n")?;
+
+  Ok(client)
+}
 pub async fn get_client<P: AsRef<Path>>(logininfo: P) -> Result<Client> {
   info!("Login...");
   let f = std::fs::File::open(logininfo)?;
@@ -94,4 +137,37 @@ async fn build_client(
   let client = builder.build().await?;
 
   Ok(client)
+}
+
+static SSO_HOSTS: LazyLock<Vec<ruma::OwnedServerName>> = LazyLock::new(|| {
+  vec![
+    ruma::owned_server_name!("mozilla.org"),
+  ]
+});
+
+/// Ask the user's account and login based on homeserver
+pub async fn new_login<P: AsRef<Path>>(
+  loginfile: P,
+) -> Result<Client> {
+  let mut rl = rustyline::DefaultEditor::new()?;
+  let uid: ruma::OwnedUserId = loop {
+    let user = match rl.readline("User: ") {
+      Ok(s) => s,
+      Err(_) => continue,
+    };
+    match user.parse() {
+      Ok(u) => {
+        break u;
+      }
+      Err(e) => {
+        eprintln!("Error: {:?}", e);
+      }
+    }
+  };
+
+  if SSO_HOSTS.iter().any(|h| h == uid.server_name()) {
+    sso_login(loginfile, uid.server_name()).await
+  } else {
+    interactive_login(loginfile, &uid).await
+  }
 }
